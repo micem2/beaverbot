0ad364cc4505a1709a1fe2052b1e95ae85bc5a64
micem2 <freakishmice@proton.me>
2024-03-22 22:20:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready. Current user is ${readyClient.user.tag}`);
+})
+
+client.login(token);
